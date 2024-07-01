82823bd9c55ab769eb72808c5b56145a36cc14c6
anish <anishmjain2003@gmail.com>
2024-07-01 17:39:41 +0530

    updated mongodb files

--- backend/db/ConnectToMongoDB.js
+++ backend/db/ConnectToMongoDB.js
@@ -1,13 +1,18 @@
 import mongoose from "mongoose";
 
-const ConnectToMongoDB = async() =>{
+const ConnectToMongoDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI);
+        await mongoose.connect(process.env.MONGO_DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            // Optionally, other options like useFindAndModify, useCreateIndex, etc.
+        });
         console.log("Connected to MongoDB");
     } catch (error) {
-        console.log("Error connecting to MongoDB",error.message)
+        console.error("Error connecting to MongoDB:", error.message);
+        // Optionally, you can throw the error or handle it further as needed.
+        throw error;
     }
 };
 
-    
-    export default ConnectToMongoDB;
+export default ConnectToMongoDB;
